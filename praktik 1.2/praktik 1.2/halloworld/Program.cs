ad366e02b7e35f728ddcd5504421da6345af2e8b
roichan-creator <aryshaputra258730@student.smkn1kandeman.sch.id>
2025-10-13 14:07:25 +0700

    Add files via upload

---
+++ praktik 1.2/praktik 1.2/halloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace halloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selamat Datang di pemrograman c#");
+            Console.WriteLine("Ini adalah tutorial belajar c# dari dasar");
+            Console.WriteLine("terima kasih sudah sudah menggunakan program ini");
+        }
+    }
+}
