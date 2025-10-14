8190b1dd66afc259027bd301b884753e86e55f98
roichan-creator <aryshaputra258730@student.smkn1kandeman.sch.id>
2025-10-14 20:11:49 +0700

    Add files via upload

---
+++ praktik 5.1/praktik 5.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("perulangan untuk menampilkan angka 1 sampai 10");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("angka ke-" + i);
+            }
+            Console.WriteLine("\nPerulangan selesai");
+        }
+    }
+}
