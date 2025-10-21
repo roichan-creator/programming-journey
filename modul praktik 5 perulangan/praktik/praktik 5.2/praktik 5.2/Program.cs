5504642aad5c4a413e92a7e40f1987f7ea9b6f9a
roichan-creator <aryshaputra258730@student.smkn1kandeman.sch.id>
2025-10-21 20:15:57 +0700

    upload file

---
+++ modul praktik 5 perulangan/praktik/praktik 5.2/praktik 5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                total = total + i;
+                Console.WriteLine(" manambahkan " + i + ", total sementara =" + total);
+            }
+            Console.WriteLine("\nHasil akhir penjumlahan adalah: " + total);
+        }
+    }
+}
